f6e0bfb845273de87940c73ad934bae6940b4f86
Nikolay <shadowbrocker@gmail.com>
2018-09-01 11:47:04 +0300

    Fixed menu decription and command list display

--- Menu.java
+++ Menu.java
@@ -56,15 +56,16 @@ public class Menu {
 
         public void select() {
 
-            System.out.println("Show books — displays the list of all books in specified location\n" +
-                    "Refill  — refills specified book storage\n" +
-                    "Add books — adds books to specified location\n" +
-                    "Remove books — removes selected books from specified location\n" +
-                    "Sort — sorts all books by user defined criteria\n" +
-                    "Search — searches for a certain book\n" +
-                    "Cash out — pays for all the books in your basket and\n" +
+            System.out.println(description());
+            System.out.println("Show books - displays the list of all books in specified location\n" +
+                    "Refill  - refills specified book storage\n" +
+                    "Add books - adds books to specified location\n" +
+                    "Remove books - removes selected books from specified location\n" +
+                    "Sort - sorts all books by user defined criteria\n" +
+                    "Search - searches for a certain book\n" +
+                    "Cash out - pays for all the books in your basket and\n" +
                     " sends them to your home library\n" +
-                    "Exit — closes Black Books");
+                    "Exit - closes Black Books");
         }
 
     }
